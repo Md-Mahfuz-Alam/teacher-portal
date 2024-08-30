dfc034416abc56c596a782b0b09ccef5874bcda7
Md Mahfuz Alam <mahfuz.alam@kreeti.com>
2024-08-30 16:38:07 +0530

    remove comments

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,6 @@ Rails.application.routes.draw do
   end
   resources :users
   resources :sessions
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
   root "users#new"
 end
